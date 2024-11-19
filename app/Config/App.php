4f0c1349dc7ef3ecefd13680a2ac22b8312afbd4
Ezk1e <tiratiraezekiel@gmail.com>
2024-11-20 07:59:01 +0800

    Initialize Code Igniter 4 v4.5.5

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:WST-TamaFlights-Summative/';
+    public string $baseURL = 'http://localhost/WST-TamaFlights-Summative/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
